45896a5ee4b16a2e048e88ef7b9c046c233ccb08
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-07-28 05:26:35 +0000

    Update run.py

--- run.py
+++ run.py
@@ -6,16 +6,15 @@ import listen as connect_listen
 import paramiko
 import logging
 import exploits
-import pyshark
 
 
 
 def build():
- os.system("pip install paramiko & pip install logging & pip install binascii & pip install hashlib & pip install scipy & pip install datetime & pip install scapy & pip install socket & pip install netifaces & pip install subprocess & pip install elementpath & pip install io & pip install sys & pip install crypto")
+ os.system("pip install crypto & paramiko & pip install logging & pip install binascii & pip install hashlib & pip install scipy & pip install datetime & pip install scapy & pip install socket & pip install netifaces & pip install subprocess & pip install elementpath & pip install io & pip install sys & pip install crypto")
 
 
 def run():
-  print("--> Documentation may be found at https://github.com/breadforhire/interpy")
+  print("--> Documentation may be found at https://github.com/breadforhire/phi-ssh")
   print("     \      ,")
   print("     l\   ,/")
   print("._   `|] /j")
